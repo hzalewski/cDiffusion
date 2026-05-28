1c43f102c449c2956481c7cafab0fc1ea6b4f0ff
hzalewski <zalewskihubert79@gmail.com>
2026-05-29 00:02:05 +0200

    Adding heap functions for KNN search

---
+++ src/knn.h
@@ -0,0 +1,15 @@
+#ifndef KNN_H
+#define KNN_H
+
+
+typedef struct {
+    double dist;
+    int idx;
+} knn_node;
+
+
+void down_heap(knn_node* heap, int i, int k_neighbors);
+void create_heap(knn_node* heap, int k_neighbors);
+void insert(knn_node* heap, int k_neighbors, int new_idx, double new_dist);
+
+#endif 
